d9e9cc1c06803917ec3594afa13144e87ecdd13d
Matheus Letra <60162176+MatheusLetra@users.noreply.github.com>
2022-02-04 09:46:11 -0300

    Update script.js

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -1,5 +1,6 @@
 let minute = 0;
 let second = 0;
+let milisec = 0;
 
 let cron;
 
@@ -10,7 +11,7 @@ const stopCron = document.querySelectorAll('.btnestilo')[1];
 stopCron.addEventListener('click', stopCronometro);
 
 const resetCron = document.querySelectorAll('.btnestilo')[2];
-stopCron.addEventListener('click', resetCronometro);
+resetCron.addEventListener('click', resetCronometro);
 
 function startCronometro() {
     stopCronometro();
@@ -21,23 +22,38 @@ function startCronometro() {
 
 function stopCronometro() {
     clearInterval(cron);
+    document.querySelectorAll('span')[0].innerHTML = formatNumber(minute);
+    document.querySelectorAll('span')[1].innerText = formatNumber(second);
+    document.querySelectorAll('span')[2].innerText = formatNumber(milisec);
 }
 
 function resetCronometro() {
     minute = 0;
     second = 0;
+    milisec = 0;
     const element = document.querySelectorAll('span');
     element.forEach((item) => {
         item.innerText = '00';
     })    
 }
 
+function formatNumber(n){
+    return n > 9 ? "" + n: "0" + n;
+}
+
 function timer() {
+    if (milisec == 60) {
+        milisec = 0;
+        second ++;
+    }
     if (second == 60) {
         second = 0;
         minute ++;
     }
 
-    document.querySelectorAll('span')[0].innerHTML = minutos;
-    document.querySelectorAll('span')[1].innerText = segundos;
+    milisec++;
+
+    document.querySelectorAll('span')[0].innerHTML = formatNumber(minute);
+    document.querySelectorAll('span')[1].innerText = formatNumber(second);
+    document.querySelectorAll('span')[2].innerText = formatNumber(milisec);
 }
